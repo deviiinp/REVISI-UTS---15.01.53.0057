19e27a78c861e2d165e4b20c1ee1b8d8afe416e7
Devi Purnama Mulyono <37098639+deviiinp@users.noreply.github.com>
2018-11-26 21:44:54 +0700

    Add files via upload

---
+++ Clustering.py
@@ -0,0 +1,24 @@
+import networkx as nx 
+import matplotlib.pyplot as plt
+
+G=nx.DiGraph() 
+
+G.add_edge('UU Nomor 22 Tahun 2001','UUD 1945') 
+G.add_edge('UU Nomor 22 Tahun 2001','Ketetapan MPR Nomor XV/MPR/1998')
+G.add_edge('UU Nomor 39 Tahun 2007','UUD 1945')
+G.add_edge('UU Nomor 39 Tahun 2007','UUD 1945')
+G.add_edge('UU Nomor 1 Tahun 2017','UU Nomor 24 Tahun 2000')
+G.add_edge('UU Nomor 18 Tahun 2017','UUD 1945') 
+G.add_edge('UU Nomor 18 Tahun 2017','UU Nomor 13 Tahun 2003')
+G.add_edge('UU Nomor 18 Tahun 2017','UU Nomor 6 Tahun 2012')
+G.add_edge('UU Nomor 2 Tahun 2018',' UUD 1945')
+G.add_edge('UU Nomor 2 Tahun 2018 ','UU Nomor 17 Tahun 2014') 
+   
+nx.draw_networkx(G)
+plt.axis('off')
+plt.show()
+
+#clustering
+G=nx.complete_graph(20)
+print(nx.clustering(G,0))
+print(nx.clustering(G))
